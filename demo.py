51323426fd718e693d7f46f414cbea64b53f736f
AlecFrancis47 <a02363384@usu.edu>
2025-01-25 03:34:52 -0700

    updated hub bg

--- demo.py
+++ demo.py
@@ -21,7 +21,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Background setup
-background = pygame.image.load('images/hub.jpg').convert()
+background = pygame.image.load('images/hubBG.png').convert()
 background_width, background_height = background.get_size()
 currentScene = Scene(background=background)
 backgrounds = ['images/background1.jpg','images/background2.jpg','images/background3.jpg','images/background4.jpg', 'images/background5.jpg']
