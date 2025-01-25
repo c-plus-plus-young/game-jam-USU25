ba29e66f0df65b4242ce76e4e593376309715993
Caleb Young <a02362954@usu.edu>
2025-01-25 01:52:06 -0700

    adding sound effects

--- music.py
+++ music.py
@@ -6,13 +6,23 @@ mixer.init()
 Function to play a song. Just use song name, not relative location
 (assuming file in music folder)
 """
-def playMusic(songName):
 
+
+def playMusic(songName):
     mixer.music.load("music/" + str(songName))
 
-    mixer.music.set_volume(0.7)
+    mixer.music.set_volume(0.5)
 
     mixer.music.play()
+
+
+def playSound(soundName):
+    my_sound = mixer.Sound("sound/" + soundName)
+
+    my_sound.play()
+
+    my_sound.set_volume(1)
+
 
 if __name__ == '__main__':
     playMusic("music4.mp3")
