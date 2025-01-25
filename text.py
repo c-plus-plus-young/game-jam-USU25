87485c29187a48a09bc81a746ed28bd512060409
Caleb Young <a02362954@usu.edu>
2025-01-24 19:30:30 -0700

    adding text file - still not working

---
+++ text.py
@@ -0,0 +1,17 @@
+import pygame, sys, time
+
+pygame.init()
+
+white = (255, 255, 255)
+"""
+prints text to screen
+time is length you want 
+"""
+def printText(text, time, x_pos, y_pos):
+    screen = pygame.display.set_mode(800, 600)
+    
+
+
+if __name__ == '__main__':
+    print('Welcome to Earth')
+    time.sleep(1)
