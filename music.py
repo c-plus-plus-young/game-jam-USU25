18bbf2e3231ba0679e3ee3a4c2b5bcf323c9dfd7
Caleb Young <a02362954@usu.edu>
2025-01-24 19:56:22 -0700

    fixed issue with music playing

--- music.py
+++ music.py
@@ -2,6 +2,10 @@ from pygame import mixer
 
 mixer.init()
 
+"""
+Function to play a song. Just use song name, not relative location
+(assuming file in music folder)
+"""
 def playMusic(songName):
 
     mixer.music.load("music/" + str(songName))
@@ -9,26 +13,6 @@ def playMusic(songName):
     mixer.music.set_volume(0.7)
 
     mixer.music.play()
-
-    while True:
-
-        print("Press 'p' to pause, 'r' to resume")
-        print("Press 'e' to exit the program")
-        query = input("  ")
-
-        if query == 'p':
-
-            # Pausing the music
-            mixer.music.pause()
-        elif query == 'r':
-
-            # Resuming the music
-            mixer.music.unpause()
-        elif query == 'e':
-
-            # Stop the mixer
-            mixer.music.stop()
-            break
 
 if __name__ == '__main__':
     playMusic("Abstraction - Fear -2HAC Volume 28- - 01 Triskaidekaphobia.mp3")
