648d6832bb840936295996f87567881f5cab9bd1
Caleb Young <a02362954@usu.edu>
2025-01-24 21:49:01 -0700

    working on text

--- text.py
+++ text.py
@@ -1,15 +1,46 @@
-import pygame, sys, time
-
-pygame.init()
+import pygame, time
 
 white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+black = (0, 0, 0)
+pygame.font.init()
+font = pygame.font.Font("fonts/Modak-Regular.ttf", 30)
 """
 prints text to screen
-time is length you want 
+length is time you want message visible
+y_pos is, you guessed it, y position of text
 """
-def printText(text, time, x_pos, y_pos):
-    screen = pygame.display.set_mode(800, 600)
-    
+
+def printText(message, y_pos, screen):
+    text = font.render(message, True, black)
+    screen.blit(text, (25, y_pos))
+    disp = True;
+    while disp:
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    disp = False
+
+"""
+Text that can be advanced by placing spacebar
+message for this should be a list, with each line
+being a separate string in said list (approximately 
+39 characters including spaces), though exact size
+will depend upon specific characters used
+"""
+def advanceableText(messages, screen):
+    # may cause issues if character is moving when text starts
+    # (character will glide when text finishes)
+    newMessage = messages.pop(0)
+    printText(newMessage, 515, screen)
+    # if len(messages) >= 1:
+    #     newMessage = messages.pop(0)
+    #     printText(newMessage, 550, screen)
+#                 if event.key == pygame.K_k:
+#                 advanceableText(["WORDS", "More words", "MMMMM MMMMM MMMMM MMMMM MMMMM MMMMM MMM"], screen)
+
 
 
 if __name__ == '__main__':
